9000eb6b8591bd4f9e682fa856ea5a84dbe5b962
RNogales <xrnogales@gmail.com>
2020-03-29 21:42:53 +0200

    Update interactive_conditional_samples.py

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -54,10 +54,10 @@ def interact_model(
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
-    with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
+    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
-        tf.set_random_seed(seed)
+        tf.compat.v1.set_random_seed(seed)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
@@ -65,27 +65,30 @@ def interact_model(
             temperature=temperature, top_k=top_k, top_p=top_p
         )
 
-        saver = tf.train.Saver()
+        saver = tf.compat.v1.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
-        while True:
-            raw_text = input("Model prompt >>> ")
-            while not raw_text:
-                print('Prompt should not be empty!')
-                raw_text = input("Model prompt >>> ")
-            context_tokens = enc.encode(raw_text)
-            generated = 0
-            for _ in range(nsamples // batch_size):
-                out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
-                for i in range(batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
-            print("=" * 80)
+        iterations = 4
+        for it in range(iterations):
+
+          raw_text = ''' iPhone XS Max and iPhone XR are resistant to accidental spills of common liquids
+          '''
+
+          context_tokens = enc.encode(raw_text)
+          generated = 0
+          for _ in range(nsamples // batch_size):
+            out = sess.run(output, feed_dict={
+                                          context: [context_tokens for _ in range(batch_size)]
+                                      })[:, len(context_tokens):]
+
+            for i in range(batch_size):
+              generated += 1
+              text = enc.decode(out[i])
+              print( it, "=" * 40, " SAMPLE ", str(generated), " ", "=" * 40)
+              # 'text' is the output, I just print it because I want to run it in a colab
+              print(text)
+          print("=" * 80)
 
 if __name__ == '__main__':
     fire.Fire(interact_model)
